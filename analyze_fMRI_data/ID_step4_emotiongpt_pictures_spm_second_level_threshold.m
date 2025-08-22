34e63051fcf8fff76bf89c3c72a2d877b20be616
Lauri301 <lauri.suominen30@gmail.com>
2025-08-22 14:43:37 +0300

    Update ID_step4_emotiongpt_pictures_spm_second_level_threshold.m

--- analyze_fMRI_data/ID_step4_emotiongpt_pictures_spm_second_level_threshold.m
+++ analyze_fMRI_data/ID_step4_emotiongpt_pictures_spm_second_level_threshold.m
@@ -5,7 +5,7 @@
 %% Threshold GPT-4V based results
 
 % Define base paths
-base_output = 'path/fmri_analysis/NAPS_fmri_gpt-4-1/second_level_gpt';
+base_output = 'path/fmri_analysis/ID_fmri_gpt-4-1/second_level_gpt';
 feature_dirs = dir(base_output);
 feature_dirs = feature_dirs([feature_dirs.isdir]); % Keep only directories
 feature_dirs = feature_dirs(~ismember({feature_dirs.name}, {'.', '..'}));
@@ -53,7 +53,7 @@ end
 
 %% Threshold human based results
 
-base_output = 'path/fmri_analysis/NAPS_fmri_gpt-4-1/second_level_human';
+base_output = 'path/fmri_analysis/ID_fmri_gpt-4-1/second_level_human';
 feature_dirs = dir(base_output);
 feature_dirs = feature_dirs([feature_dirs.isdir]); % Keep only directories
 feature_dirs = feature_dirs(~ismember({feature_dirs.name}, {'.', '..'})); % Exclude . and ..
@@ -98,4 +98,5 @@ for i = 1:length(feature_dirs)
     else
         fprintf('SPM.mat file not found for feature: %s\n', feature);
     end
+
 end
